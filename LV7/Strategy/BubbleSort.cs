83fe608caf84cde8debd5cdd37927c9d07c99ab3
LukaKuterovac <luka.kuterovac@gmail.com>
2022-06-02 16:11:46 +0200

    Task 1 commit. Implemented and tested BubbleSort class.

--- LV7/Strategy/BubbleSort.cs
+++ LV7/Strategy/BubbleSort.cs
@@ -6,7 +6,14 @@ namespace Strategy
     {
         public override void Sort(double[] array)
         {
-            //implementation missing!
+            int arrayLength = array.Length;
+            for (int i = 0; i < arrayLength - 1; i++)
+            {
+                for (int j = 0; j < arrayLength - 1; j++)
+                {
+                    if (array[j] > array[j + 1]) Swap(ref array[j], ref array[j + 1]);
+                }
+            }
         }
     }
 }
--- LV7/Strategy/Program.cs
+++ LV7/Strategy/Program.cs
@@ -6,7 +6,18 @@ namespace Strategy
     {
         static void Main(string[] args)
         {
-            
+            Task1();
+        }
+
+        static void Task1()
+        {
+            double[] sequence = new double[] { 2, 5, 7, 1, 3};
+            NumberSequence numberSequence = new NumberSequence(sequence);
+            SortStrategy strategy1 = new BubbleSort();
+            numberSequence.SetSortStrategy(strategy1);
+            Console.WriteLine(numberSequence);
+            numberSequence.Sort();
+            Console.WriteLine(numberSequence);
         }
     }
 }
